5a7dafc77f18112ffeeb0a95cf525a39f4d17646
Ishan Agrawal <ishan.agrawal90@gmail.com>
2016-07-04 01:27:10 +0800

    history db first commit

---
+++ main.rb
@@ -0,0 +1,36 @@
+require 'sinatra'
+require 'redis'
+require 'json'
+
+$redis = Redis.new(:host => "localhost", :port => 6379)
+
+get '/object/:key' do
+  ts = params['timestamp'] || Time.now.to_i
+  key = params['key']
+  return 601, custom_error("No key found") if key == "" || key.nil?
+  latest = $redis.zrevrangebyscore params['key'], ts, 0, :limit => [0, 1]
+  return 602, custom_error("No value found with key: #{key}") if latest.empty?
+  return latest
+end
+
+post '/object' do
+  begin
+    payload = JSON.parse(request.body.read)
+    print "payload #{payload}"
+    return 603, custom_error("Bad request body") if payload.nil? || payload.empty?
+  rescue JSON::ParserError => e
+    return 603, custom_error("Bad request body")
+  end
+  key, value = payload.first
+  ts = Time.now.to_i
+  latest = $redis.zrevrangebyscore key, ts, 0, :limit => [0, 1]
+  $redis.zadd key, ts, value unless latest == value
+end
+
+not_found do
+  'Sorry, this route is not available'
+end
+
+def custom_error str
+  {"error" => str}.to_json
+end
